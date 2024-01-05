8af65a8b746632d214719fbf9499a85d8b3956da
Sergei Calabonga <dev@calabonga.net>
2024-01-05 12:12:15 +0700

    PagedList nuget used

--- src/Calabonga.UnitOfWork/EnumerablePagedListExtensions.cs
+++ src/Calabonga.UnitOfWork/EnumerablePagedListExtensions.cs
@@ -19,7 +19,7 @@ public static class EnumerablePagedListExtensions
     /// <param name="indexFrom">The start index value.</param>
     /// <returns>An instance of the inherited from <see cref="IPagedList{T}"/> interface.</returns>
     public static IPagedList<T> ToPagedList<T>(this IEnumerable<T> source, int pageIndex, int pageSize, int indexFrom = 0)
-        => PagedList.Create<T>(source, pageIndex, pageSize, indexFrom);
+        => PagedList.Create(source, pageIndex, pageSize, indexFrom);
 
     /// <summary>
     /// Converts the specified source to <see cref="IPagedList{T}"/> by the specified <paramref name="converter"/>, <paramref name="pageIndex"/> and <paramref name="pageSize"/>
@@ -28,15 +28,9 @@ public static class EnumerablePagedListExtensions
     /// <typeparam name="TResult">The type of the result</typeparam>
     /// <param name="source">The source to convert.</param>
     /// <param name="converter">The converter to change the <typeparamref name="TSource"/> to <typeparamref name="TResult"/>.</param>
-    /// <param name="pageIndex">The page index.</param>
-    /// <param name="pageSize">The page size.</param>
-    /// <param name="indexFrom">The start index value.</param>
     /// <returns>An instance of the inherited from <see cref="IPagedList{T}"/> interface.</returns>
     public static IPagedList<TResult> ToPagedList<TSource, TResult>(
-        this IEnumerable<TResult> source,
-        Func<IEnumerable<TResult>, IEnumerable<TResult>> converter,
-        int pageIndex,
-        int pageSize,
-        int indexFrom = 0)
-        => PagedList.Create<TSource, TResult>(source, converter, pageIndex, pageSize, indexFrom);
+        this IPagedList<TSource> source,
+        Func<IEnumerable<TSource>, IEnumerable<TResult>> converter)
+        => PagedList.Create<TSource, TResult>(source, converter);
 }
