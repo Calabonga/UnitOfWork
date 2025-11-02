b39b2972987bab7125771a0548909f2db0d1ca6d
Sergei Calabonga <dev@calabonga.net>
2025-11-02 09:00:38 +0700

    6.2.0-beta.1
    * FactoryUnitOfWork added
    * ServiceLifetime selection added

---
+++ src/Calabonga.UnitOfWork/UnitOfWorkFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Calabonga.UnitOfWork;
+
+/// <summary>
+/// CALABONGA Warning: do not remove sealed
+/// Represents the default implementation of the <see cref="T:IUnitOfWork"/> and <see cref="T:IUnitOfWork{TContext}"/> interface.
+/// </summary>
+/// <typeparam name="TContext">The type of the db context.</typeparam>
+public sealed class UnitOfWorkFactory<TContext> : IUnitOfWorkFactory<TContext>
+    where TContext : DbContext
+{
+    public UnitOfWorkFactory(IDbContextFactory<TContext> factory) => DbContext = factory.CreateDbContext();
+
+    public TContext DbContext { get; }
+
+    public IUnitOfWork CreateUnitOfWork() => new UnitOfWork<TContext>(DbContext);
+
+    public void Dispose() => DbContext.Dispose();
+}
